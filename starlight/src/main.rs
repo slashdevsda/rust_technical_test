81a46ca692feaa69dd21b4f868c384426f977c0f
slashdevsda <slashdevsda@gmail.com>
2021-08-09 15:32:05 +0200

    tests subject sample and removes comments/prints

--- starlight/src/main.rs
+++ starlight/src/main.rs
@@ -14,13 +14,16 @@ fn can_permute(idx: usize, seq: &Vec<u8>) -> bool {
         .into_iter()
         .filter(|u| **u == 1 as u8)
         .count() == 0 {
-            println!("can permute? (index {}) yes! tail is {:?}", idx, seq.split_at(idx + 2).1);
+        // i + 1 should be `1`
+        // i + 2 ... i + N should be `0`
             true
     } else {
         false
     }
 }
 
+/// permutes `seq[idx]` to `symbol` by going recursively to each required steps.
+/// always returns the number of steps achieved.
 fn permute_to(symbol: u8, idx: usize, seq: &mut Vec<u8>) -> usize {
     let mut steps: usize = 0;
 
@@ -35,20 +38,15 @@ fn permute_to(symbol: u8, idx: usize, seq: &mut Vec<u8>) -> usize {
             .1
             .into_iter()
             .any(|u| *u == 1 as u8) {
-                // println!("yoho ({}) {} {:?} {:?}", symbol, idx, seq, seq.split_at(idx + 2).1);
-                // for (offset, _i) in seq.clone().split_at(idx + 2).1.into_iter().enumerate() {
                     for offset in idx+2..seq.len() {
-                    // a check
-                    // println!("idx: {}, offset: {}, idx+offset, {}", idx, offset, idx+offset);
                     steps += permute_to(0, offset, seq);
                 }
             }
 
     };
     if can_permute(idx, seq){
-    // permutation allowed
+        // permutation allowed, so we proceed.
         seq[idx] = symbol;
-        // println!("permut {:?}", seq);
         steps + 1
     } else {
         0
@@ -75,7 +73,6 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let target = input_line.trim_end().to_string();
-    println!("{} to {}", start, target);
 
     let mut start: Vec<u8> = start
         .into_bytes()
@@ -104,10 +101,9 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
+    #[test] 
     fn test_can_permute_1() {
         let seq = vec![0, 1, 0, 1];
-        // assert_eq!(10, value);
         assert!(can_permute(0, &seq) == false);
         assert!(can_permute(1, &seq) == false);
         assert!(can_permute(3, &seq) == true);
@@ -117,7 +113,6 @@ mod tests {
     #[test]
     fn test_can_permute_2() {
         let seq = vec![0, 1, 0, 0];
-        // assert_eq!(10, value);
         assert!(can_permute(0, &seq) == true);
         assert!(can_permute(1, &seq) == false);
         assert!(can_permute(2, &seq) == false);
@@ -125,11 +120,16 @@ mod tests {
     }
 
     #[test]
-    fn test_subject() {
+    fn test_subject_simple() {
+        let mut seq = vec![1,0,1,0,1,0];
+        let target = vec![0,1,0,1,0,1];
+        assert_eq!(starlight(&mut seq, &target), 26);
+    }
 
+    #[test]
+    fn test_subject() {
         let mut seq = vec![1,1,0,0,1,0,0,1,0,0,0];
         let target = vec![1,0,0,0,0,1,1,0,0,1,1];
-        // assert_eq!(10, value);
         assert_eq!(starlight(&mut seq, &target), 877);
     }
 }
